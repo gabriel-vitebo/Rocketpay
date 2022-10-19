0d2c7a0348e5da1e1bca8ed15867c577cdb1a0df
Gabriel Vitebo <vitebo@outlook.com>
2022-10-19 11:58:46 -0300

    changing cards design and adding new cards

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
-    mastercard: ["#DF6F29", "#C69347"],
+    mastercard: ["#315881", "#DFA43B"],
+    elo: ["#2D57F2", "#CBC652"],
     default: ["black", "gray"],
   }
   ccBgColor01.setAttribute("fill", colors[type] [0])
